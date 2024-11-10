f665845819d61910cd546b62ff5bc911ef5aad48
Yves EWOMBA-JOCKTANE <jewomba@hotmail.com>
2024-11-10 21:23:24 +0100

    mise en place du bouton retour haut de page

--- js/main.js
+++ js/main.js
@@ -3,4 +3,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Code pour les statistiques interactives
   });
-  
+
+
+// Bouton de retour en haut de la page
+let mybutton = document.getElementById("topPage");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;  
+  document.documentElement.scrollTop = 0; }
